c891110ac5a3577f87860ec43adc98976e0c8701
Adam <adamirzakan@gmail.com>
2024-03-20 10:26:19 +0300

    Создан новый проект, удален сториборд

---
+++ Food/Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food
+//
+//  Created by Адам on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
